1a42f17ce63b2c24f9d7968e04e79bc92823b22e
Son Haechang <sonhaechang@Sonui-MacBookPro.local>
2023-05-16 00:22:36 +0900

    feat: Create ToDo's state management logic
    1. TextInput component를 사용해서 입력받은 내용을 상태 관리하는 로직 생성

--- App.tsx
+++ App.tsx
@@ -1,8 +1,10 @@
 import React, { useState } from 'react';
 import {
+	Alert,
 	StatusBar,
 	StyleSheet,
 	Text,
+	TextInput,
 	TouchableOpacity,
 	View,
 } from 'react-native';
@@ -12,8 +14,24 @@ StatusBar.setBarStyle('light-content');
 
 function App(): JSX.Element {
 	const [working, setWorking] = useState<boolean>(true);
+	const [text, setText] = useState<string>('');
+	const [toDos, setToDos] = useState<object>({});
+
 	const traval = () => setWorking(false);
 	const work = () => setWorking(true);
+	const onChangeText = (e: any) => setText(e);
+	const addToDo = () => {
+		if (text === '') {
+			return;
+		}
+
+		const newToDos = Object.assign({}, toDos, {
+			[Date.now()]: { text, work: working },
+		})
+
+		setToDos(newToDos);
+		setText('');
+	};
 
 	return (
 		<View style={styles.container}>
@@ -41,6 +59,18 @@ function App(): JSX.Element {
 					</Text>
 				</TouchableOpacity>
 			</View>
+
+			<TextInput 
+				onChangeText={onChangeText}
+				onSubmitEditing={addToDo}
+				returnKeyType='done'
+				value={text}
+				placeholderTextColor='#525868'
+				placeholder={
+					working ? 'Add a To Do' : 'where do you want to go?'
+				}
+				style={styles.input} 
+			/>
 		</View>
 	);
 }
@@ -61,6 +91,16 @@ const styles = StyleSheet.create({
 	btnText: {
 		fontSize: 38,
 		fontWeight: '600',
+	}, 
+
+	input: {
+		backgroundColor: '#232937',
+		paddingVertical: 15,
+		paddingHorizontal: 20,
+		borderRadius: 30,
+		marginTop: 20,
+		fontSize: 18,
+		color: 'white',
 	}
 });
 
