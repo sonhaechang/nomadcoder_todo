05e906f6034a39ce32687d06746bdeccb0671106
Son Haechang <sonhaechang@Sonui-MacBookPro.local>
2023-05-16 14:53:12 +0900

    feat: Save ToDos in async-storage
    1. npm install @react-native-async-storage/async-storage
    2. AsyncStorage에서 데이터 불러오고 저장하기

--- App.tsx
+++ App.tsx
@@ -1,6 +1,7 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import {
 	Alert,
+	ScrollView,
 	StatusBar,
 	StyleSheet,
 	Text,
@@ -8,30 +9,59 @@ import {
 	TouchableOpacity,
 	View,
 } from 'react-native';
+import AsyncStorage from '@react-native-async-storage/async-storage';
 import { theme } from './colors';
 
 StatusBar.setBarStyle('light-content');
 
+const STORAGE_KEY = "@toDos";
+
 function App(): JSX.Element {
 	const [working, setWorking] = useState<boolean>(true);
 	const [text, setText] = useState<string>('');
-	const [toDos, setToDos] = useState<object>({});
+	const [toDos, setToDos] = useState<{} | any>({});
 
 	const traval = () => setWorking(false);
 	const work = () => setWorking(true);
 	const onChangeText = (e: any) => setText(e);
-	const addToDo = () => {
-		if (text === '') {
-			return;
+
+	const loadToDos = async () => {
+		try {
+			const s: {} | any = await AsyncStorage.getItem(STORAGE_KEY);
+			s && setToDos(JSON.parse(s));
+		} catch(error) {
+			console.log(error);
 		}
-
-		const newToDos = Object.assign({}, toDos, {
-			[Date.now()]: { text, work: working },
-		})
-
-		setToDos(newToDos);
-		setText('');
 	};
+
+	const addToDo = async () => {
+		try {
+			if (text !== '') {
+				const newToDos = {
+					...toDos, 
+					[Date.now()]: { text, working }
+				}
+		
+				setToDos(newToDos);
+				await saveToDos(newToDos);
+				setText('');		
+			}
+		} catch(error) {
+			console.log(error);
+		}
+	};
+
+	const saveToDos = async (toSave: any) => {
+		try {
+			await AsyncStorage.setItem(STORAGE_KEY, JSON.stringify(toSave));
+		} catch(error) {
+			console.log(error);
+		}
+	};
+
+	useEffect(() => {
+		loadToDos();
+	}, []);
 
 	return (
 		<View style={styles.container}>
@@ -41,7 +71,7 @@ function App(): JSX.Element {
 					<Text 
 						style={{ 
 							...styles.btnText, 
-							color: working ? 'white' : theme.gray 
+							color: working ? 'white' : theme.grey 
 						}}
 					>
 						Work
@@ -52,7 +82,7 @@ function App(): JSX.Element {
 					<Text 
 						style={{ 
 							...styles.btnText,
-							color: !working ? 'white' : theme.gray 
+							color: !working ? 'white' : theme.grey 
 						}}
 					>
 						Travel
@@ -64,13 +94,27 @@ function App(): JSX.Element {
 				onChangeText={onChangeText}
 				onSubmitEditing={addToDo}
 				returnKeyType='done'
+				clearButtonMode='always'
+				placeholder={working ? 'Add a To Do' : 'where do you want to go?'}
 				value={text}
-				placeholderTextColor='#525868'
-				placeholder={
-					working ? 'Add a To Do' : 'where do you want to go?'
-				}
 				style={styles.input} 
 			/>
+
+			<ScrollView>
+				{Object.keys(toDos).map(key => 
+					toDos[key].working === working ?
+					(
+						<View 
+							key={key}
+							style={styles.toDo}
+						>
+							<Text style={styles.toDoText}>
+								{toDos[key].text}
+							</Text>
+						</View> 
+					) : null
+				)}
+			</ScrollView>
 		</View>
 	);
 }
@@ -94,13 +138,29 @@ const styles = StyleSheet.create({
 	}, 
 
 	input: {
-		backgroundColor: '#232937',
+		backgroundColor: 'white',
 		paddingVertical: 15,
 		paddingHorizontal: 20,
-		borderRadius: 30,
-		marginTop: 20,
-		fontSize: 18,
+		borderRadius: 10,
+		marginVertical: 20,
+		fontSize: 15,
+	},
+
+	toDo: {
+		flexDirection: 'row',
+		alignItems: 'center',
+		justifyContent: 'space-between',
+		backgroundColor: theme.toDoBg,
+		marginBottom: 20,
+		paddingVertical: 15,
+		paddingHorizontal: 20,
+		borderRadius: 10,
+	},
+
+	toDoText: {
 		color: 'white',
+		fontSize: 15,
+		fontWeight: '500',
 	}
 });
 
